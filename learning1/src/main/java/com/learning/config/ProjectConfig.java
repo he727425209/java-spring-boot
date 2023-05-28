e1205379c9691ed9e9a177a89345d04837405b80
Kevin He <kevinhe@Kevins-MacBook-Pro-92.local>
2023-05-28 15:58:11 +0800

    learning 1

---
+++ learning1/src/main/java/com/learning/config/ProjectConfig.java
@@ -0,0 +1,25 @@
+package com.learning.config;
+
+import com.learning.beans.Vehicle;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ProjectConfig {
+    @Bean
+    Vehicle vehicle() {
+        var vehicle = new Vehicle();
+        vehicle.setName("Audi 8");
+        return vehicle;
+    }
+
+    @Bean
+    String hello() {
+        return "hello world";
+    }
+
+    @Bean
+    Integer number() {
+        return 16;
+    }
+}
