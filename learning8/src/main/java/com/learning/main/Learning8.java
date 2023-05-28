d01225fd224988becc3ff693b83c6628785b84cb
Kevin He <kevinhe@Kevins-MacBook-Pro-92.local>
2023-05-28 17:55:06 +0800

    learning 8

---
+++ learning8/src/main/java/com/learning/main/Learning8.java
@@ -0,0 +1,19 @@
+package com.learning.main;
+
+import com.learning.beans.Vehicle;
+import com.learning.config.ProjectConfig;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.Random;
+import java.util.function.Supplier;
+
+public class Learning8 {
+    public static void main(String[] args) {
+
+        var context = new ClassPathXmlApplicationContext("beans.xml");
+        Vehicle vehicle = context.getBean(Vehicle.class);
+        System.out.println("Vehicle name from Spring Context is: " + vehicle.getName());
+
+    }}
