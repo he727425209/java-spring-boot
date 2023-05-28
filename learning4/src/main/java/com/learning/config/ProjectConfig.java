78c7789f6083b0f20b303f77a4ae7075eff7e082
Kevin He <kevinhe@Kevins-MacBook-Pro-92.local>
2023-05-28 16:22:41 +0800

    learning 4

---
+++ learning4/src/main/java/com/learning/config/ProjectConfig.java
@@ -0,0 +1,31 @@
+package com.learning.config;
+
+import com.learning.beans.Vehicle;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class ProjectConfig {
+    @Bean(name = "audiVehicle")
+    Vehicle vehicle1() {
+        var vehicle = new Vehicle();
+        vehicle.setName("Audi");
+        return vehicle;
+    }
+
+    @Bean(value = "hondaVehicle")
+    Vehicle vehicle2() {
+        var vehicle = new Vehicle();
+        vehicle.setName("Honda");
+        return vehicle;
+    }
+
+    @Primary
+    @Bean("ferariVehicle")
+    Vehicle vehicle3() {
+        var vehicle = new Vehicle();
+        vehicle.setName("Ferari");
+        return vehicle;
+    }
+}
