dd8f477bd728c3f0256d37ac4440940fa67f5f9f
Kevin He <kevinhe@Kevins-MBP-92.lan>
2023-05-29 10:59:34 +0800

    learning 10

---
+++ learning10/src/main/java/com/learning/config/ProjectConfig.java
@@ -0,0 +1,26 @@
+package com.learning.config;
+
+import com.learning.beans.Person;
+import com.learning.beans.Vehicle;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class ProjectConfig {
+    @Bean
+    Vehicle vehicle() {
+        Vehicle vehicle = new Vehicle();
+        vehicle.setName("Toyota");
+        return vehicle;
+    }
+
+    @Bean
+    Person person(Vehicle vehicle) {
+        Person person = new Person();
+        person.setName("Lucy");
+        person.setVehicle(vehicle);
+        return person;
+    }
+}
