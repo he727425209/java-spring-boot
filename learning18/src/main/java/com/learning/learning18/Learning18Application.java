ede30d97d7c208556be987f0c3324e4f421cf6d5
Kevin He <kevinhe@587232706.guest.umich.net>
2023-05-30 16:52:26 +0800

    learning 18

---
+++ learning18/src/main/java/com/learning/learning18/Learning18Application.java
@@ -0,0 +1,13 @@
+package com.learning.learning18;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Learning18Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Learning18Application.class, args);
+	}
+
+}
