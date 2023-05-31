3a8417cb2fcb7ebee492f71b13e5518a819c7bd6
Kevin He <kevinhe@587232439.guest.umich.net>
2023-05-31 17:04:46 +0800

    learning 21

---
+++ learning21/src/test/java/com/learning/learning21/LearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.learning21;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
