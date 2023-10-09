50ba9e2e8b0a19bb47aa1e7c3b7c970b552e8766
Kevin He <kevinhe@Kevins-MBP-92.attlocal.net>
2023-10-09 19:44:32 -0400

    learning 27

--- learning27/src/main/java/com/learning/learning27/config/ProjectSecurityConfig.java
+++ learning27/src/main/java/com/learning/learning27/config/ProjectSecurityConfig.java
@@ -1,11 +1,22 @@
 package com.learning.learning27.config;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration(proxyBeanMethods = false)
-@ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
-public class SpringBootWebSecurityConfiguration {
-
-
+@Configuration
+public class ProjectSecurityConfig {
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests) -> {
+            ((AuthorizeHttpRequestsConfigurer.AuthorizedUrl)requests.anyRequest()).permitAll();
+        });
+        http.formLogin(Customizer.withDefaults());
+        http.httpBasic(Customizer.withDefaults());
+        return (SecurityFilterChain)http.build();
+    }
 }
---
+++ learning27/src/main/java/com/learning/learning27/model/Holiday.java
@@ -0,0 +1,14 @@
+package com.learning.learning27.model;
+
+import lombok.Data;
+
+@Data
+public class Holiday {
+    private final String day;
+    private final String reason;
+    private final Type type;
+
+    public enum Type{
+        FESTIVAL, FEDERAL
+    }
+}
