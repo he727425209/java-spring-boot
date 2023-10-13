ca84d2d11dadd682d8cd1a83f486638058b7ee36
Kevin He <kevinhe@Kevins-MBP-92.attlocal.net>
2023-10-12 21:25:58 -0400

    learning 30

--- learning30/src/main/java/com/learning/learning30/controller/GlobalExceptionController.java
+++ learning30/src/main/java/com/learning/learning30/controller/GlobalExceptionController.java
@@ -8,7 +8,9 @@ import org.springframework.web.servlet.ModelAndView;
 @Slf4j
 @ControllerAdvice
 public class GlobalExceptionController {
-    @ExceptionHandler(Exception.class)
+    @ExceptionHandler({NullPointerException.class,
+    ArrayIndexOutOfBoundsException.class,
+    RuntimeException.class})
     public ModelAndView exceptionHandler(Exception exception) {
         ModelAndView error = new ModelAndView();
         error.setViewName("error");
